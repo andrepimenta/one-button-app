bad7e15feb7da8f113fba30a485f7be619efef23
Andre Pimenta <andrepimenta7@gmail.com>
2016-06-04 01:18:19 +0100

    trying peerjs

--- public/bundle.js
+++ public/bundle.js
@@ -1,36 +1,21 @@
-var Peer = window.SimplePeer;
-var p = new Peer({ initiator: location.hash === '#1', trickle: false, config: { iceServers: [ { url: 'stun:23.21.150.121' } ] } })
-
-/*
-var p = new Peer(
-{
-  initiator: location.hash === '#1',
-  channelConfig: {},
-  channelName: '<random string>',
-  config: { iceServers: [ { url: 'stun:23.21.150.121' } ] },
-  reconnectTimer: false,
-  sdpTransform: function (sdp) { return sdp },
-  stream: false,
-  trickle: true
-});
-*/
-p.on('error', function (err) { console.log('error', err) })
-
-p.on('signal', function (data) {
-  console.log('SIGNAL', JSON.stringify(data))
-  document.querySelector('#outgoing').textContent = JSON.stringify(data)
-})
+var peer = new Peer({key: 'umreiafrr9wo2yb9'});
 
 document.querySelector('form').addEventListener('submit', function (ev) {
-  ev.preventDefault()
-  p.signal(JSON.parse(document.querySelector('#incoming').value))
+	ev.preventDefault()
+
+	var conn = peer.connect(document.querySelector('#incoming').value);
+	conn.on('open', function(){
+		conn.send('hi!');
+	});
+
 })
 
-p.on('connect', function () {
-  alert("CONNECT");
-  p.send('whatever' + Math.random())
-})
+peer.on('open', function(id) {
+  console.log('My peer ID is: ' + id);
+});
 
-p.on('data', function (data) {
- alert('data: ' + data)
-})
+peer.on('connection', function(conn) {
+  conn.on('data', function(data){
+    console.log(data);
+  });
+});
