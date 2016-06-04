2f6d1ffc9076c053e031c740f5d283512a871e8a
Andre Pimenta <andrepimenta7@gmail.com>
2016-06-04 01:50:32 +0100

    working

--- public/bundle.js
+++ public/bundle.js
@@ -5,9 +5,8 @@ document.querySelector('form').addEventListener('submit', function (ev) {
 
 	var conn = peer.connect(document.querySelector('#incoming').value);
 	conn.on('open', function(){
-		conn.send('hi!');
+		conn.send([{type: 'url', url: 'http://www.google.pt'}, {type: 'url', url: 'http://www.facebook.com'}]);
 	});
-
 })
 
 peer.on('open', function(id) {
@@ -16,6 +15,10 @@ peer.on('open', function(id) {
 
 peer.on('connection', function(conn) {
   conn.on('data', function(data){
-    console.log(data);
+	  var i=0;
+	  for(i=0; i<data.length;i++){
+		  if(data[i].type==='url')
+			window.open(data[i].url, '_blank');
+	  }
   });
 });
